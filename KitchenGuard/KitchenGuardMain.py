64d977f48e40127c9315d6aed8a5aa5f4d1c0e84
emilandersen28 <91263866+emilandersen28@users.noreply.github.com>
2023-05-10 12:56:29 +0200

    0

--- KitchenGuard/KitchenGuardMain.py
+++ KitchenGuard/KitchenGuardMain.py
@@ -1,7 +1,10 @@
 from KitchenGuardZ2M_Controller import KitchenGuardZ2M
+from KitchenGuardWebClient import WebClient
 
 # Start MQTT client and loop, which may activate controller
 KitchenGuardZ2M.start()
+
+WebClient.run()
 
 
 
